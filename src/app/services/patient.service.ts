ccd4b5278ef01f1a50937e904685ed51d468fbef
hallier <ahallier@isara.fr>
2021-12-23 12:11:44 +0100

    Fix : urls correct urls in patient.service.ts

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -7,28 +7,26 @@ import {environment} from "../../environments/environment";
 @Injectable()
 export class PatientService {
 
-  private apiServerUrl = environment.apiBaseUrl;
-
   constructor(private http: HttpClient) {}
 
   public getPatientById(id: number): Observable<Patient> {
-    return this.http.get<Patient>('${this.apiServerUrl}/patients/${id}');
+    return this.http.get<Patient>(`${environment.apiBaseUrl}/patients/${id}`);
   }
 
   public getPatients(): Observable<Patient[]> {
-    return this.http.get<Patient[]>('${this.apiServerUrl}/patients');
+    return this.http.get<Patient[]>(`${environment.apiBaseUrl}/patients`);
   }
 
   public addPatient(patient: Patient): Observable<Patient> {
-    return this.http.post<Patient>('${this.apiServerUrl}/patients', patient);
+    return this.http.post<Patient>(`${environment.apiBaseUrl}/patients`, patient);
   }
 
   public updatePatient(id: number, patient: Patient): Observable<Patient> {
-    return this.http.put<Patient>('${this.apiServerUrl}/patients/${id}', patient);
+    return this.http.put<Patient>(`${environment.apiBaseUrl}/patients/${id}`, patient);
   }
 
   public deletePatient(id: number): Observable<void> {
-    return this.http.delete<void>('${this.apiServerUrl}/patients/${id}');
+    return this.http.delete<void>(`${environment.apiBaseUrl}/patients/${id}`);
   }
 
 }
