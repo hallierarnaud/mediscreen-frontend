910a7b587e5226b09c9fc1616919f72b3a8fd517
hallier <arnaud.hallier@hotmail.fr>
2022-03-25 18:11:09 +0100

    Feat : security implement jwt authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'http://localhost:8084/api/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -16,9 +16,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
-      username,
-      password
+    return this.http.post(AUTH_API + 'login?username='+username+'&password='+password, {
     }, httpOptions);
   }
 
